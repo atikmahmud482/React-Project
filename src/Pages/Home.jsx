2c169443db9576e166790fd4a593f8081055f2e5
atikmahmud482 <atikmahmud482@gmail.com>
2025-02-04 23:22:33 +0600

    categories section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,22 @@
+import Categories from "../Category";
 import Nav from "../components/nav";
 
 const Home = () => {
   return (
     <div className="bg-slate-200 w-full min-h-screen">
-      <Nav></Nav>
+      <Nav />
+      <div className="overflow-x-auto whitespace-nowrap p-4">
+        <div className="flex gap-15 justify-center">
+          {Categories.map((item) => (
+            <div
+              key={item.id}
+              className="bg-white rounded-lg shadow-md p-3 w-30 h-30 flex flex-col items-center justify-center hover:shadow-lg transition-shadow duration-300 cursor-pointer text-center hover:bg-green-200">
+              <div className="text-4xl">{item.image}</div>
+              <div className="mt-2 text-sm font-semibold">{item.name}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@ import { LuShoppingBag } from "react-icons/lu";
 const Nav = () => {
   return (
     <div className="w-full h-[100px] flex justify-between items-center px-8">
-      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl">
+      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl hover:bg-slate-50">
         <MdFastfood className="w-[30px] h-[30px] text-green-500" />
       </div>
       <form
-        className="w-[70%] h-[60px] bg-white flex items-center px-5 gap-5 rounded-md shadow-md"
+        className="w-[70%] h-[60px] bg-white flex items-center px-5 gap-5 rounded-md shadow-md hover:bg-slate-50"
         action="">
         <IoSearch className="text-green-500 w-[25px] h-[25px]" />
         <input
@@ -17,11 +17,11 @@ const Nav = () => {
           placeholder="Search Item..."
           className=" w-[100%] outline-none text-[20px]"></input>
       </form>
-      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl relative">
+      <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl relative hover:bg-amber-50">
         <span className="absolute top-0 right-2 text-green-500 font-bold">
           0
         </span>
-        <LuShoppingBag className="w-[30px] h-[30px] text-green-500" />
+        <LuShoppingBag className="w-[30px] h-[30px] text-green-500 hover:bg-slate-50" />
       </div>
     </div>
   );
